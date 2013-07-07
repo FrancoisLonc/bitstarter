14d927c6bc5567cb139cadc6620c7a052dc4fdcc
François Lonc <francois_nant@yahoo.fr>
2013-07-07 15:42:49 +0200

    first web page from file

--- web.js
+++ web.js
@@ -1,12 +1,16 @@
 var express = require('express');
 
+var filesystem_lib = require('fs');
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var filecontent = filesystem_lib.readFileSync("index.html", 'utf8');
+  response.send(filecontent);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
