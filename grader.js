857a3e06ae68edf758fe4788ca342de29462855a
François Lonc <francois_nant@yahoo.fr>
2013-07-09 18:10:35 +0200

    operational version with temp file

--- grader.js
+++ grader.js
@@ -3,25 +3,23 @@
 var filesystem_lib = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
-var restler_lib = require('restler');
+var http_lib = require('http');
+var url_lib = require('url');
 
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-var URL_DEFAULT = "https://mighty-fjord-9128.herokuapp.com"
+var URL_DEFAULT = "http://mighty-fjord-9128.herokuapp.com"
+var TMP_URL_FILE = "temp_index.html";
 
-var checkError;
+var error, printResponse, htmlString;
 
 var assertFileExists = function (infile) {
   var instr = infile.toString();
   if (!filesystem_lib.existsSync(instr)) {
-    console.log("%s does not exist. Exiting.", instr);
+    console.error("%s does not exist. Exiting.", instr);
     process.exit(1);
   }
   return instr;
-};
-
-var cheerioHtmlFile = function (htmlfile) {
-  return cheerio.load(filesystem_lib.readFileSync(htmlfile));
 };
 
 var loadChecks = function (checksfile) {
@@ -29,7 +27,7 @@ var loadChecks = function (checksfile) {
 };
 
 var checkHtml = function (htmlString, checksfile) {
-  var checks, out, present;
+  var $, checks, out, present;
 
   $ = cheerio.load(htmlString);
   checks = loadChecks(checksfile).sort();
@@ -48,16 +46,37 @@ var clone = function (fn) {
 
 var checkJson, outJson, toCheck;
 
-checkError = function (result, response) {
-  if (result instanceof Error) {
-    console.error("Could not reach url %s", program.url);
-    console.error(response.message);
-    process.exit(1);
-  }
-  else {
-     return result; 
-  }
-}
+error = function(response) {
+  console.error("Could not reach url %s", program.url);
+  console.error(response.message);
+  process.exit(1);
+};
+
+printResponse = function (result) {
+  result.setEncoding('utf8');
+  result.on('data', function (chunk) {
+    filesystem_lib.writeFile("temp_index.html", chunk, function (error) {
+      if (error) {
+        console.error("Couldn't write file");
+        process.exit(1);
+      }
+    });
+  }).on('end', function () {
+    filesystem_lib.readFile("temp_index.html", function (error, data) {
+      var checkJson, outJson;
+      if (error) {
+        console.error("Couldn't read file");
+        process.exit(1);
+      }
+      else {
+        checkJson = checkHtml(data, program.checks);
+        outJson = JSON.stringify(checkJson, null, 4);
+        
+        console.log(outJson);
+      }
+    });
+  });
+};
 
 if(require.main == module)
 {
@@ -72,10 +91,10 @@ clone(assertFileExists), HTMLFILE_DEFAULT)
   }
 
   for (index = 2; index < program.rawArgs.length; index += 2) {
-    if (toCheck === undefined && program.rawArgs[index] !== '--checks') {
+    if (toCheck === undefined && !(/-c/.test(program.rawArgs[index]))) {
       toCheck = program.rawArgs[index];
     }
-    else if (program.rawArgs[index] !== '--checks') {
+    else if (!(/-c/.test(program.rawArgs[index]))) {
       console.error("wrong argument combination");
       program.help();
     }
@@ -83,20 +102,17 @@ clone(assertFileExists), HTMLFILE_DEFAULT)
 
   toCheck = toCheck || '--file';
 
-  if (toCheck === '--file') {
+  if (/-f/.test(toCheck)) {
     htmlString = filesystem_lib.readFileSync(program.file);
+    checkJson = checkHtml(htmlString, program.checks);
+    outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
   }
   else {
-    htmlString = restler_lib.get(program.url).on('complete', checkError);
+    clientRequest = http_lib.get(program.url, printResponse)
+     .on('error', error);
   }
- 
-  console.log(htmlString);
-
-  checkJson = checkHtml(htmlString, program.checks);
-  outJson = JSON.stringify(checkJson, null, 4);
-  console.log(outJson);
 }
-else
-{
+else {
   exports.checkHtmlFile = checkHtmlFile;
 }
